1902222ffb156b2d3ad3a2ec6ce5189aea578cf6
YH Yang <yhuiyang@gmail.com>
2013-05-16 16:56:30 +0800

    Add water shader.
    See http://www.andengine.org/forums/post48773.html for detail.

--- src/com/yhsoftlab/shadertest/LauncherActivity.java
+++ src/com/yhsoftlab/shadertest/LauncherActivity.java
@@ -1,6 +1,7 @@
 package com.yhsoftlab.shadertest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LauncherActivity extends Activity {
@@ -8,5 +9,7 @@ public class LauncherActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.startActivity(new Intent(this, WaterShaderActivity.class));
+		this.finish();
 	}
 }
