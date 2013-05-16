4cb42df66ffc8a6e30c07e32970d27ee5e683fc9
YH Yang <yhuiyang@gmail.com>
2013-05-16 10:58:56 +0800

    Update manifest.
    1. Change preferred install location.
    2. Request OpenGL ES 2.0.
    3. Remove unused stuff.

--- src/com/yhsoftlab/shadertest/LauncherActivity.java
+++ src/com/yhsoftlab/shadertest/LauncherActivity.java
@@ -1,22 +1,12 @@
 package com.yhsoftlab.shadertest;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class LauncherActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_launcher);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.launcher, menu);
-		return true;
-	}
-
 }
